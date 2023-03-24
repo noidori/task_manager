14f5f60d7a0e11f11e2f8a14360b1881ad9b87a8
no1dori <idrisov201@gmail.com>
2023-03-24 19:32:08 +0300

    Changes and admin.py for displaying custom fields.

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Tag, Task
 
 
@@ -11,34 +12,37 @@ task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
 
 @admin.register(Tag, site=task_manager_admin_site)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("id", "title")
 
 
 @admin.register(Task, site=task_manager_admin_site)
 class TaskAdmin(admin.ModelAdmin):
-    pass
-
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ["username", "email", "first_name", "last_name", "role"]
-    list_filter = ["role"]
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
-        (
-            "Permissions",
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "user_permissions",
-                )
-            },
-        ),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-        ("Custom fields", {"fields": ("role",)}),
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "creation_date",
+        "modification_date",
+        "due_date",
+        "status",
+        "author",
+        "performer",
+        "priority",
+    )
+    list_editable = (
+        "status",
+        "priority",
+        "performer",
     )
 
 
-task_manager_admin_site.register(User, UserAdmin)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ["username", "email", "first_name", "last_name", "role"]
+    list_filter = ["role"]
+    add_fieldsets = (*UserAdmin.add_fieldsets, ("Custom fields", {"fields": ("role",)}))
+    fieldsets = (*UserAdmin.fieldsets, ("Custom field", {"fields": ("role",)}))
+    list_editable = ("role",)
+
+
+task_manager_admin_site.register(User, CustomUserAdmin)
